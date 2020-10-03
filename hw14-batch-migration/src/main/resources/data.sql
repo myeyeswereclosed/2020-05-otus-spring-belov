50ae877cc0b63a258d7f117649f564925c3e9130
myeyeswereclosed <belovi-tech@mail.ru>
2020-10-03 20:26:32 +0300

    hw14-batch-migration some load

--- hw14-batch-migration/src/main/resources/data.sql
+++ hw14-batch-migration/src/main/resources/data.sql
@@ -10,5 +10,10 @@ insert into book_genre(book_id, genre_id) values (1, 1), (2, 1), (2,2);
 insert into comment(book_id, text) values
     (1, 'Good book!'), (1, 'Super!'), (3, 'About birds)');
 
+-- some load to check it's ok to migrate authors in one batch)
+insert into author(first_name, last_name)
+    select 'FirstName_' || x.id, 'LastName_' || x.id
+    from generate_series(1, 100000) as x(id);
+
 commit;
 
